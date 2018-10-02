79c61f535de89645aa56c72ae7f82ed8787501bc
munnsmunns <mmunns16@gmail.com>
2018-10-02 14:50:22 -0500

    Fix array_merge and fix memory leaks in array_merge and the tests

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,35 +1,56 @@
 #include <stdlib.h>
 
 #include "array_merge.h"
+#include "../mergesort/mergesort.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
 	int sum = 0;
 	for(int i = 0; i < num_arrays; i++) {
 	sum += sizes[i];
 	}
-	int destination[sum];
-	int i = 0;
-	int j = 0;
+
+	if(sum <= 0) {
+	  int* result = (int*) calloc(1, sizeof(int));
+	  return result;
+	}
+	
+	int* destination = (int*) calloc(sum, sizeof(int));
+
+	// combine all values into one array, including duplicates
 	int k = 0;
-	while(i < sizes[0] && j < sizes[1]) {
-		if(values[0][i] < values[1][j]) {
-			destination[k] = values[0][i];
-			i++;
-		} else {
-			destination[k] = values[1][j];
-			j++;
-		}
-		k++;
+	for(int i = 0; i < num_arrays; i++) {
+	  for(int j = 0; j < sizes[i]; j++) {
+	    destination[k] = values[i][j];
+	    k++;
+	  }
 	}
-	while(i < sizes[0]) {
-		destination[k] = values[0][i];
-		i++;
-		k++;
+
+	// sort values
+	mergesort(sum, destination);
+
+	// remove duplicates
+	int duplicates = 0;
+	for(int i = sum - 1; i > 0; i--){
+	  // set duplicates out of bounds
+	  if (destination[i] == destination[i-1]) {
+	    destination[i] = -1;
+	    duplicates++;
+	  }
 	}
-	while(j < sizes[1]) {
-		destination[k] = values[1][j];
-		j++;
-		k++;
+
+	int* finalDestination = (int*) calloc(sum - duplicates + 1, sizeof(int));
+
+	for(int i = 0, j = 1; i < sum; i++) {
+	  if(destination[i] != -1) {
+	    finalDestination[j] = destination[i];
+	    j++;
+	  }
 	}
-	return destination;
+
+	free(destination);
+
+	// put the number of elements at index 0
+	finalDestination[0] = sum - duplicates;
+	
+	return finalDestination;
 }
